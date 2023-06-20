192002aa08f0f12b715b515e71eba4416122f4bc
Ishika Mahajan <mahajish@sheridancollege.ca>
2023-06-20 19:32:17 +0530

    Merge origin/master
    Conflicts:
    	src/practice/Practice.java

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+import java.util.*;
+/**
+ *
+ * @author Lenovo
+ */
+public class TestUserprofile {
+    
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.println("Enter your name: ");
+String name = input.nextLine();
+System.out.println("Select a genre from the following: Comedy, Drama, Action, Mystery: ");
+String gen = input.nextLine();
+System.out.println("Your user profile was created");
+}
+}
